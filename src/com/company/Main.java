92d970511fbce3c3bd3b47b4657ad214d47f6541
Tofik Mussa <tofahub@gmail.com>
2018-05-31 14:18:32 -0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myNumber=2;
+        double myOtherNumber=1.7938;
+        System.out.println("The value of my numbers are " + myNumber + " and  "+ myOtherNumber);
+    }
+}
